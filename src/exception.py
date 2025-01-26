4c81d88fb4f97edfed8dcdff618e49e4015cee93
Nehayj100 <nehayj100@tamu.edu>
2025-01-25 18:19:10 -0600

    added EDA

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,7 @@
 import sys
 # used to control python runtime env
-import logging
+from src.logger import logging
+
 
 def error_message_details(error, error_detail:sys):
     _, _, exc_tb = error_detail.exc_info() 
